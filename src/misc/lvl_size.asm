9ee1ec82e614717c41daa5a2d1ed34af712b0904
Selbi182 <shadow93x@gmail.com>
2025-07-29 15:25:15 +0200

    Fixed a background bug at the start of NHP

--- src/misc/lvl_size.asm
+++ src/misc/lvl_size.asm
@@ -1,7 +1,7 @@
 ;	dc.w (unused), left boundary, right boundary
 ;	dc.w upper boundary, lower boundary, default Y-camera position
 
-	dc.w $0004, $0000, $2185-SCREEN_XCORR	; GHZ 1
+	dc.w $0004, $0002, $2185-SCREEN_XCORR	; GHZ 1
 	dc.w $0000, $0300, $0060
 	dc.w $0004, $02C0, $1E40	; GHZ 2
 	dc.w $0000, $0100, $0060
